7c27bd5d38254523ad9b512f82950c2f052bef2a
Viki Flendiansyah <vfsyah@gmail.com>
2023-11-27 12:30:18 +0700

    feat: add service

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./component/Hero";
+import Service from "./component/Service";
 
 export default function Home() {
   return (
     <div className="bg-white">
       <Hero />
+      <Service />
     </div>
   );
 }
